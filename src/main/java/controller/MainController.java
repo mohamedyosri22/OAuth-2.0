42c4024a95dae10163eeff9d3106798a24b70b13
mhmdyouali <mhmd.you@yahoo.com>
2023-02-06 14:11:23 +0200

    OAuth first try & failed after redirecting

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/auth")
+    public String getLogin(){
+        return "welcome";
+    }
+}
